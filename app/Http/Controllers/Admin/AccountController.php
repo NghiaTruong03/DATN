e6e8c4749defc1735eb45895ab423dc863c90c8e
Nghia Truong <truongnghia620@gmail.com>
2022-05-03 01:05:40 +0700

    update admin function

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -5,39 +5,93 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
-    public function index(){
+    public function indexUser(){
+        $user_list = User::withTrashed()->get();
+        // $account_list = User::all();
+        return view('admin.account.index',compact('user_list'));
+    }
+
+    public function indexStaff(){
+        $staff_list = User::all();
+        return view('admin.account.staff',compact('staff_list'));
+    }
+    public function editStaff($id)
+    {   
         
-        $account_list = User::all();
-        return view('admin.account.index',compact('account_list'));
+        // $user_account = User::where('role','!=','0');   
+        // return view('admin.account.edit',compact('user_account'));
     }
-
-    public function viewProfile(){
-
-    }
-
     public function editAccount($id)
-    {
-        $account_edit = User::find($id);
-        return view('admin.account.edit',compact('account_edit'));
+    {   
+        $user_account = User::find($id);
+        if($user_account->role != 0){
+            $user_id = $id;
+        }else{
+            $user_account = User::withTrashed()->where('id', '=' ,$id)->first();
+            $user_id= $user_account->id;      
+        }
+        return view('admin.account.edit',compact('user_account','user_id'));
     }
 
 
     public function updateAccount(Request $request,$id ){
-        $account_update = User::find($id);
-        $account_update->update($request->all());
-        if($account_update){
-            return redirect()->route('account.index')->with('success','Cập nhật thành công');
+        $rules = [
+            'name' => 'required|unique:users,name,'.$id,
+            'email' => 'required|unique:users,email,'.$id,
+            'phoneNumber' => 'required|numeric'
+
+        ];
+
+        $messages = [
+            'name.required' => 'Tên không được để trống',
+            'email.required' => 'Email không được để trống',
+            'email.unique' => 'Email đã tồn tại',
+            'phoneNumber.required' => 'Số điện thoại không được để trống',
+            'phoneNumber.numeric' => 'Số điện thoại phải ở dạng số'
+
+        ];
+        $request->validate($rules,$messages);
+        
+        $user_account = User::find($id);
+        //kiem tra tai khoan la khach hang hay nhan vien
+        if($user_account->role != 0){
+            $user_account->update($request->except('status'));
+            return redirect()->route('account.staff.index')->with('success','Cập nhật thành công');
         }else{
-            dd('Cập nhật thất bại');
+            if($request->status) {
+                if($request->status==2){
+                     User::withTrashed()->where('id', '=' ,$id)->restore();
+                }else{
+                     User::withTrashed()->where('id', '=' ,$id)->delete();
+                }
+             }
+             $account_update = User::withTrashed()->where('id', '=' ,$id)->update($request->only('name','phoneNumber','email'));
+             return redirect()->route('account.user.index')->with('success','Cập nhật thành công');
         }
+
+
+       
+
     }
     
+    public function lockAccount($id){
+        $delete_user = User::find($id);
+        //khi delete voi user($id) thi user do thuc hien update thoi gian tai cot deleted_at
+        $delete_user->delete();
+        return redirect()->back();
+    }
+
     public function deleteAccount($id){
         $delete_user = User::find($id);
-        $delete_user->delete();
+        if(Auth::user()->id == $id){
+            return redirect()->route('account.staff.index')->with('error','Không thể xóa tài khoản này');
+        }
+        //su dung forceDelete toan bo ban ghi
+        $delete_user->forceDelete();
         return redirect()->back();
     }
 }
