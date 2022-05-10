83cd8f41e3e8438790546ea423982b65fd7a3235
vu-duc-anh-99 <demaon2010@gmail.com>
2022-05-10 21:20:23 +0700

    feature/order/use jquery to add value to form

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -64,7 +64,11 @@ $(document).ready(function() {
 
 })
 
-
+$('#cua-form').on('change', 'input,textarea', function() {
+    let data = $(this).val()
+    let inputName = $(this).attr('name');
+    $('#vnpay_form input[name=' + inputName + ']').val(data);
+});
 
 function requireLogin() {
     alert("Yêu cầu đăng nhập để mua hàng");
